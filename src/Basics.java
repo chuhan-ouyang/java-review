e4d28d81beefa2a0d4aafcc7fb7d83a6e2c0c5ef
chuhan <chuhan.ouyang@gmail.com>
2023-10-20 15:50:45 -0400

    Create basics review

---
+++ src/Basics.java
@@ -0,0 +1,13 @@
+public class Basics {
+    public static void main(String[] args) {
+        int integer = 10; // 4 bytes
+        char character = 'x';
+        long longVal= 5L; // 8 bytes
+        byte byteVal =-128; // 1 byte signed
+        short shortVal = -32768; // 2 byte signed
+        boolean boolVal = true;
+        boolean boolVal1 = false;
+        double doubleVal = 2.0;
+        float myFloat = 2.0f;
+    }
+}
