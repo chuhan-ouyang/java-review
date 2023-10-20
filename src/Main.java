5094de5bb95569087106b11c15f9cb98e70e7e08
chuhan <chuhan.ouyang@gmail.com>
2023-10-20 15:51:31 -0400

    Create basics review

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] a = new int[2];
+
+        for (int element : a) {
+            System.out.println(element);
+        }
+    }
+}
