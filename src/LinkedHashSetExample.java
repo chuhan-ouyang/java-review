6967daad7bf1c934f237fb4df3d0fcf4139cdfb2
chuhan <chuhan.ouyang@gmail.com>
2023-10-22 12:27:09 -0400

    Add LinkedHashSet Notes

--- src/LinkedHashSetExample.java
+++ src/LinkedHashSetExample.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class LinkedHashSetExample {
+import java.util.*;
+import java.io.*;
+
+/**
+ * Maintain doubly linked list to keep inserted element in order
+ */
+public class LinkedHashSetExample {
+    public static void main(String[] args) {
+        // Add
+        LinkedHashSet<String> ll = new LinkedHashSet<String>();
+        ll.add("A");
+        ll.add("B");
+        ll.add("C");
+        ll.add("D");
+        printList(ll);
+
+        // Contains
+        System.out.println(ll.contains("C"));
+
+        // Remove
+        ll.remove("C");
+
+        // RemoveIf
+        ll.removeIf(x -> x.compareTo("A") <= 0);
+
+        printList(ll);
+    }
+
+    public static void printList(LinkedHashSet<String> ll) {
+        for (String s : ll) {
+            System.out.println(s);
+        }
+        System.out.println();
+    }
 }
